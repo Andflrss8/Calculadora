ee42cbd74ae14c98e6739f5402ae85baa17a14ef
bravo <andflores2006@gmail.com>
2026-01-30 15:49:56 -0600

    Version propia con suma y boton reset

---
+++ Calculadora/frmCalculadora.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Calculadora
+{
+    partial class frmCalculadora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtVariableA = new TextBox();
+            txtVariableB = new TextBox();
+            Calcular = new Button();
+            btnReset = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(53, 38);
+            label1.Name = "label1";
+            label1.Size = new Size(77, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Variable A";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(53, 123);
+            label2.Name = "label2";
+            label2.Size = new Size(76, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Variable B";
+            // 
+            // txtVariableA
+            // 
+            txtVariableA.Location = new Point(57, 70);
+            txtVariableA.Name = "txtVariableA";
+            txtVariableA.Size = new Size(264, 27);
+            txtVariableA.TabIndex = 2;
+            // 
+            // txtVariableB
+            // 
+            txtVariableB.Location = new Point(55, 181);
+            txtVariableB.Name = "txtVariableB";
+            txtVariableB.Size = new Size(266, 27);
+            txtVariableB.TabIndex = 3;
+            // 
+            // Calcular
+            // 
+            Calcular.Location = new Point(387, 128);
+            Calcular.Name = "Calcular";
+            Calcular.Size = new Size(94, 29);
+            Calcular.TabIndex = 4;
+            Calcular.Text = "Calcular";
+            Calcular.UseVisualStyleBackColor = true;
+            Calcular.Click += Calcular_Click;
+            // 
+            // btnReset
+            // 
+            btnReset.Location = new Point(392, 184);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(94, 29);
+            btnReset.TabIndex = 5;
+            btnReset.Text = "Reset";
+            btnReset.UseVisualStyleBackColor = true;
+            btnReset.Click += btnReset_Click;
+            // 
+            // frmCalculadora
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnReset);
+            Controls.Add(Calcular);
+            Controls.Add(txtVariableB);
+            Controls.Add(txtVariableA);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "frmCalculadora";
+            Text = "frmCalculadora";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtVariableA;
+        private TextBox txtVariableB;
+        private Button Calcular;
+        private Button btnReset;
+    }
+}
